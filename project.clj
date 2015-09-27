ee05b9049464b31b3f3f32144ba44a179af39dc1
Luis <luis@decomputed.com>
2015-09-27 14:40:26 +0200

    Makes project build

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject project-euler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Solutions for Project Euler in Clojure"
+  :url "https://github.com/decomputed/projectEuler/tree/Clojure"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
